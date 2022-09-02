ed09935597677635fb0cf0fa7fcdc2e1593847c0
Douglas Lombard <douglaslombard29@gmail.com>
2022-09-02 14:13:25 +0200

    Added a function to add a new person

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const nameInput = document.querySelector("#nameInput");
 const amountInput = document.querySelector("#amount");
 const radioOptions = document.querySelectorAll("input[name='option']");
 const submitBtn = document.querySelector("#submit-btn");
+const personName = document.querySelector(".name");
+const personAmount = document.querySelector(".amount");
+const columnsContainer = document.querySelector(".column"); //CHANGE THIS BACK TO "ALL"
 
 // Get the current date and display it in the nav
 const today = new Date();
@@ -12,14 +15,67 @@ const currentDate = `${today.getFullYear()}/${
 }/${+today.getDate()}`;
 dateElement.textContent = currentDate;
 
-submitBtn.addEventListener("click", (e) => {
+// submitBtn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   let selectedOption;
+//   for (const radioBtn of radioOptions) {
+//     if (radioBtn.checked) {
+//       selectedOption = radioBtn.value;
+//       break;
+//     }
+//   }
+//   console.log(selectedOption);
+// });
+
+form.addEventListener("submit", submittingForm);
+
+// Create an object from the form data
+function submittingForm(e) {
   e.preventDefault();
-  let selectedOption;
-  for (const radioBtn of radioOptions) {
-    if (radioBtn.checked) {
-      selectedOption = radioBtn.value;
-      break;
-    }
-  }
-  console.log(selectedOption);
-});
+  const formData = new FormData(e.target);
+
+  const formDataObj = Object.fromEntries(formData.entries());
+  console.log(formDataObj);
+  addPerson(formDataObj);
+}
+
+function addPerson(person) {
+  //   personName.textContent = person.name;
+  //   personAmount.textContent = `R${person.number}.00`;
+
+  const personContainer = document.createElement("div");
+  personContainer.classList.add("person");
+  columnsContainer.append(personContainer);
+
+  const personName = document.createElement("h5");
+  personName.textContent = person.name;
+  personName.classList.add("name");
+  personContainer.append(personName);
+
+  const personInfo = document.createElement("div");
+  personInfo.classList.add("person-info");
+  personContainer.append(personInfo);
+
+  const personAmount = document.createElement("p");
+  personAmount.textContent = `R${person.number}.00`;
+  personAmount.classList.add("amount");
+  personInfo.append(personAmount);
+
+  const buttons = document.createElement("div");
+  buttons.classList.add("buttons");
+  personInfo.append(buttons);
+
+  const checkmark = document.createElement("p");
+  checkmark.classList.add(
+    "fa-sharp",
+    "fa-solid",
+    "fa-square-check",
+    "checkmark"
+  );
+  buttons.append(checkmark);
+  const remove = document.createElement("p");
+  remove.classList.add("fa-sharp", "fa-solid", "fa-rectangle-xmark", "remove");
+  buttons.append(remove);
+}
+
+console.log(columnsContainer);
