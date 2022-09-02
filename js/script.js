f8e96deef21add8c5944b760fb29aab3d19f4adc
Douglas Lombard <douglaslombard29@gmail.com>
2022-09-02 10:35:05 +0200

    Added the current date

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+const dateElement = document.querySelector(".date");
+const today = new Date();
+const currentDate = `${today.getFullYear()}/${
+  today.getMonth() + 1
+}/${+today.getDate()}`;
 
+dateElement.textContent = currentDate;
