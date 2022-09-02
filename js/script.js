eaeaefdd12a1096cdc67cbd80c8fe289c07bd4f0
Douglas Lombard <douglaslombard29@gmail.com>
2022-09-02 11:07:48 +0200

    Added the option to check which radio button was selected in the form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 const dateElement = document.querySelector(".date");
+const form = document.querySelector(".form");
+const nameInput = document.querySelector("#nameInput");
+const amountInput = document.querySelector("#amount");
+const radioOptions = document.querySelectorAll("input[name='option']");
+const submitBtn = document.querySelector("#submit-btn");
+
+// Get the current date and display it in the nav
 const today = new Date();
 const currentDate = `${today.getFullYear()}/${
   today.getMonth() + 1
 }/${+today.getDate()}`;
-
 dateElement.textContent = currentDate;
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let selectedOption;
+  for (const radioBtn of radioOptions) {
+    if (radioBtn.checked) {
+      selectedOption = radioBtn.value;
+      break;
+    }
+  }
+  console.log(selectedOption);
+});
